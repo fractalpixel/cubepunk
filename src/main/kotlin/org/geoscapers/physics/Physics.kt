4a06effd85056bbe5eebff4c6c4adcbff37a8eca
fractalpixel <->
2019-06-13 16:56:01 +0300

    Ode4j physics is a pain and doesn't seem to work correctly, hold my energy drink while I DIY the physics.

---
+++ src/main/kotlin/org/geoscapers/physics/Physics.kt
@@ -0,0 +1,15 @@
+package org.geoscapers.physics
+
+import org.mistutils.service.ServiceBase
+import org.mistutils.service.ServiceProvider
+
+class Physics: ServiceBase() {
+
+    override fun doInit(serviceProvider: ServiceProvider?) {
+
+    }
+
+    override fun doShutdown() {
+
+    }
+}
